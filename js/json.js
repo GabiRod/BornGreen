ea43d4ef00e29e2e4e9ed7337fe22e75184b763e
GabiRod <43204938+GabiRod@users.noreply.github.com>
2019-05-27 09:35:13 +0200

    json

--- js/json.js
+++ js/json.js
@@ -1,7 +1,17 @@
-var firstName = document.getElementsByClassName("first")[0],
-    first;
+var firstName = document.getElementsByClassName("first");
+var next = document.getElementsByClassName("next");
+/*
 localStorage.setItem("first", "im first");
 
 first = localStorage.getItem("first");
 
 firstName.innerHTML = first;
+*/
+
+next.mouseout;
+
+function () {
+    var name = firstName.value;
+};
+
+console.log(name);
